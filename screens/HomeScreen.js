0525bbb027dc490e16e38a23215208df961c2062
abnerrivera <abner500edrey@gmail.com>
2022-06-06 00:06:05 -0500

    create screens

---
+++ screens/HomeScreen.js
@@ -0,0 +1,33 @@
+//react
+import { useEffect } from 'react';
+//ui
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  //get api
+  const loadTasks = async () => {
+    
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, [])
+  
+  return (
+    <View style={styles.container}>
+      <Text>home</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
