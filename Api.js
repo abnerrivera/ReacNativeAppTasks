047c90d73c874ed7b350b4915b2e097fe2840ea1
abnerrivera <abner500edrey@gmail.com>
2022-06-08 19:44:38 -0500

    refactor get api

--- Api.js
+++ Api.js
@@ -1,6 +1,8 @@
-const getTasks = async () => {
+
+const API = 'http://MIIP/tasks'
+
+export const getTasks = async () => {
   //la peticion al estar en local debe apuntar a la ip del pc para que sea reconocida
-  const response = await fetch('http://MIIP/tasks');
-  const data = await response.json();
-  return data;
+  const response = await fetch(API);
+  return response.json();
 };
